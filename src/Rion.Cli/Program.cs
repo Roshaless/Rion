2eabac08aeca61b602fa1b2f462e71eabbd332e9
Roshaless <Roshaless@outlook.com>
2024-10-12 19:50:11 +0800

    Add Rion.Cli project.

---
+++ src/Rion.Cli/Program.cs
@@ -0,0 +1,7 @@
+// Copyright (c) 2024 Roshaless
+// All rights reserved.
+//
+// This source code is distributed under an MIT license.
+// LICENSE file in the root directory of this source tree.
+
+System.Console.WriteLine("Hello, World!");
