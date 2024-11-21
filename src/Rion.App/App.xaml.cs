be845f7ab605ddcc87e04ce12eeb598eee21c583
Roshaless <Roshaless@outlook.com>
2024-11-21 21:36:15 +0800

    Update and clean up code and configuration files
    Refactored `App.xaml.cs` by removing unused namespaces and updated the project's `.editorconfig` for better readability. Also, cleaned up unnecessary using directives in `MainWindow.xaml.cs` and adjusted the formatting in `Rion.Core.csproj`.

--- src/Rion.App/App.xaml.cs
+++ src/Rion.App/App.xaml.cs
@@ -1,20 +1,16 @@
-﻿// Copyright (c) 2024 Roshaless
+// Copyright (c) 2024 Roshaless
 // All rights reserved.
-//
+// 
 // This source code is distributed under an MIT license.
 // LICENSE file in the root directory of this source tree.
 
-using System.Configuration;
-using System.Data;
 using System.Windows;
 
-namespace Rion.App
-{
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
-    {
-    }
+namespace Rion.App;
 
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
 }
--- src/Rion.App/MainWindow.xaml.cs
+++ src/Rion.App/MainWindow.xaml.cs
@@ -1,19 +1,10 @@
-﻿// Copyright (c) 2024 Roshaless
+// Copyright (c) 2024 Roshaless
 // All rights reserved.
 //
 // This source code is distributed under an MIT license.
 // LICENSE file in the root directory of this source tree.
 
-using System.Text;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Rion.App
 {
