f3c94386f717c8ad48440625653a4979eb9e9503
Roshaless <Roshaless@outlook.com>
2024-12-09 23:47:02 +0800

    refactor(cli): improve code structure and add dynamic dependency attribute
    - Restructure the main program to use a static class and method for better organization
    - Add [DynamicDependency] attribute to the Main method for potential dynamic loading
    - Improve code readability by using consistent indentation and line breaks
    - Simplify some code blocks for better maintainability

--- src/Rion.Cli/Program.cs
+++ src/Rion.Cli/Program.cs
@@ -6,6 +6,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.IO;
 using System.Linq;
 
@@ -14,119 +15,125 @@ using CommandLine;
 using Rion.Core;
 using Rion.Core.Hashing;
 
-
-Parser.Default.ParseArguments<Options>(args).WithParsed(options =>
+internal static class Program
 {
-    var toWriteRst = new List<(string, IRStringTable)>();
-    var writeToJson = new List<(string, IRStringTable)>();
-
-    foreach (var file in options.InputFiles)
+    [DynamicDependency(DynamicallyAccessedMemberTypes.All, typeof(Options))]
+    internal static void Main(string[] args)
     {
+        Parser.Default.ParseArguments<Options>(args).WithParsed(options =>
+        {
+            var toWriteRst = new List<(string, IRStringTable)>();
+            var writeToJson = new List<(string, IRStringTable)>();
+
+            foreach (var file in options.InputFiles)
+            {
+                try
+                {
+                    var fileStream = File.OpenRead(file);
+                    var firstByte = fileStream.ReadByte();
+                    {
+                        fileStream.Dispose();
+                    }
+
+                    if (firstByte is 0x52)
+                    {
+                        writeToJson.Add((file, RFile.ReadAsRecord(file)));
+                    }
+                    else
+                    {
+                        // Try to read as json
+
+                        var rst = RConvert.FromJsonFile(file);
+                        if (rst != null) toWriteRst.Add((file, rst));
+                        else Console.WriteLine($"Unsupported file type: {file}.");
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine($"Invalid input file: {file}.");
+                    continue;
+                }
+            }
+
+            LoadHashes(
+                Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "hashes"),
+                writeToJson.Select(x => x.Item2.Metadata.HashAlgorithm).Distinct().ToArray());
+
+
+            DoConvert(writeToJson, (x =>
+            {
+                var outputPath = ChangeExtExt(x.Item1, ".json");
+                RConvert.ToJsonFile(outputPath, x.Item2);
+                return outputPath;
+            }));
+
+            DoConvert(toWriteRst, (x =>
+            {
+                var outputPath = ChangeExtExt(x.Item1, ".stringtable");
+                RFile.Write(outputPath, x.Item2);
+                return outputPath;
+            }));
+        });
+    }
+
+    static void DoConvert(List<(string, IRStringTable)> collection, Func<(string, IRStringTable), string> convert)
+    {
+        foreach (var item in collection)
+        {
+            Console.WriteLine($"input: {item.Item1}");
+
+            try
+            {
+                var outputPath = convert(item);
+                Console.WriteLine($"output: {outputPath}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"error: {e.Message}");
+            }
+        }
+    }
+
+
+    static void LoadHashes(string hashesDir, params IRSTHashAlgorithm[] hashAlgorithms)
+    {
+        if (hashAlgorithms.Length == 0)
+            return;
+
+        if (Directory.Exists(hashesDir) is false)
+            return;
+
         try
         {
-            var fileStream = File.OpenRead(file);
-            var firstByte = fileStream.ReadByte();
+            foreach (var path in Directory.EnumerateFiles(hashesDir))
             {
-                fileStream.Dispose();
-            }
-
-            if (firstByte is 0x52)
-            {
-                writeToJson.Add((file, RFile.ReadAsRecord(file)));
-            }
-            else
-            {
-                // Try to read as json
-
-                var rst = RConvert.FromJsonFile(file);
-                if (rst != null) toWriteRst.Add((file, rst));
-                else Console.WriteLine($"Unsupported file type: {file}.");
+                foreach (var hashAlgorithm in hashAlgorithms)
+                {
+                    try
+                    {
+                        RHashtable.LoadFromStrings(File.ReadAllLines(path), hashAlgorithm);
+                    }
+                    catch (Exception)
+                    {
+                        continue;
+                    }
+                }
             }
         }
         catch
         {
-            Console.WriteLine($"Invalid input file: {file}.");
-            continue;
+            // ignored
         }
     }
 
-    LoadHashes(
-        Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "hashes"),
-        writeToJson.Select(x => x.Item2.Metadata.HashAlgorithm).Distinct().ToArray());
-
-
-    DoConvert(writeToJson, (x =>
+    static string ChangeExtExt(string path, string ext)
     {
-        var outputPath = ChangeExtExt(x.Item1, ".json");
-        RConvert.ToJsonFile(outputPath, x.Item2);
-        return outputPath;
-    }));
-
-    DoConvert(toWriteRst, (x =>
-    {
-        var outputPath = ChangeExtExt(x.Item1, ".stringtable");
-        RFile.Write(outputPath, x.Item2);
-        return outputPath;
-    }));
-});
-
-static void DoConvert(List<(string, IRStringTable)> collection, Func<(string, IRStringTable), string> convert)
-{
-    foreach (var item in collection)
-    {
-        Console.WriteLine($"input: {item.Item1}");
-
-        try
-        {
-            var outputPath = convert(item);
-            Console.WriteLine($"output: {outputPath}");
-        }
-        catch (Exception e)
-        {
-            Console.WriteLine($"error: {e.Message}");
-        }
+        return Path.ChangeExtension(Path.GetFullPath(path), ext);
     }
-}
 
-
-static void LoadHashes(string hashesDir, params IRSTHashAlgorithm[] hashAlgorithms)
-{
-    if (hashAlgorithms.Length == 0)
-        return;
-
-    if (Directory.Exists(hashesDir) is false)
-        return;
-
-    try
+    public sealed class Options
     {
-        foreach (var path in Directory.EnumerateFiles(hashesDir))
-        {
-            foreach (var hashAlgorithm in hashAlgorithms)
-            {
-                try
-                {
-                    RHashtable.LoadFromStrings(File.ReadAllLines(path), hashAlgorithm);
-                }
-                catch (Exception)
-                {
-                    continue;
-                }
-            }
-        }
+        [Value(0, Required = true, HelpText = "The input files.")]
+        public required IEnumerable<string> InputFiles { get; set; }
     }
-    catch
-    {
-        // ignored
-    }
-}
-
-static string ChangeExtExt(string path, string ext)
-{
-    return Path.ChangeExtension(Path.GetFullPath(path), ext);
-}
-
-file class Options
-{
-    [Value(0, Required = true, HelpText = "The input files.")]
-    public required IEnumerable<string> InputFiles { get; set; }
 }
