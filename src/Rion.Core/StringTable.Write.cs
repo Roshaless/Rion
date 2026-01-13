d7e8ba11892891008ce61217c6546c653ce22c07
Roshaless <Roshaless@outlook.com>
2026-01-13 22:13:05 +0800

    Add the missing comments

--- src/Rion.Core/StringTable.Write.cs
+++ src/Rion.Core/StringTable.Write.cs
@@ -8,46 +8,54 @@ using System.IO;
 
 using Microsoft.Win32.SafeHandles;
 
-#pragma warning disable IDE0063
-
 namespace Rion.Core;
 
 public partial class StringTable
 {
 
+    /// <summary>
+    /// Write a string table to the specified output stream.
+    /// </summary>
+    /// <param name="output">The output stream to write to.</param>
+    /// <param name="table">The string table to write.</param>
     public static void Write(Stream output, IStringTable table)
     {
-        using (var writer = new StringTableFileWriter(table))
-        {
-            writer.WriteTo(output);
-        }
+        using var writer = new StringTableFileWriter(table);
+        writer.WriteTo(output);
     }
 
-    ///
+    /// <summary>
+    /// Write a string table to the specified output file stream.
+    /// </summary>
+    /// <param name="output">The output stream to write to.</param>
+    /// <param name="table">The string table to write.</param>
     public static void Write(FileStream output, IStringTable table)
     {
-        using (var writer = new StringTableFileWriter(table))
-        {
-            writer.WriteTo(output);
-        }
+        using var writer = new StringTableFileWriter(table);
+        writer.WriteTo(output);
     }
 
+    /// <summary>
+    /// Write a string table to the specified handle at the given file offset.
+    /// </summary>
+    /// <param name="handle">The handle to write to.</param>
+    /// <param name="fileOffset">The file offset to write to.</param>
+    /// <param name="table">The string table to write.</param>
     public static void Write(SafeFileHandle handle, int fileOffset, IStringTable table)
     {
-        using (var writer = new StringTableFileWriter(table))
-        {
-            writer.WriteTo(handle, fileOffset);
-        }
+        using var writer = new StringTableFileWriter(table);
+        writer.WriteTo(handle, fileOffset);
     }
 
+    /// <summary>
+    /// Write a string table to the specified output file path.
+    /// </summary>
+    /// <param name="outputPath">The output file path to write to.</param>
+    /// <param name="table">The string table to write.</param>
     public static void Write(string outputPath, IStringTable table)
     {
-        using (var file = Internal.FileOperations.OpenOrCreate(outputPath))
-        {
-            using (var writer = new StringTableFileWriter(table))
-            {
-                writer.WriteTo(file);
-            }
-        }
+        using var file = Internal.FileOperations.OpenOrCreate(outputPath);
+        using var writer = new StringTableFileWriter(table);
+        writer.WriteTo(file);
     }
 }
